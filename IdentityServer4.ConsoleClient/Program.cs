e1d3dea1259a2026e417d27f6e15c560d06274a8
Matt Nygren <MattNygren_7@hotmail.com>
2018-12-21 11:13:38 -0800

    baseline working version identity server with client call

--- IdentityServer4.ConsoleClient/Program.cs
+++ IdentityServer4.ConsoleClient/Program.cs
@@ -1,11 +1,19 @@
 ﻿using IdentityModel.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace IdentityServer4.ConsoleClient
 {
     class Program
     {
+        //TO TEST:
+        //1. First run the identity server projec
+        //2. Next, run the api
+        //3. Lastly, run the console app
         public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
 
         private static async Task MainAsync()
@@ -31,6 +39,42 @@ namespace IdentityServer4.ConsoleClient
 
             Console.WriteLine(tokenResponse.Json);
             Console.WriteLine("/n/n");
+
+            //consume our client api
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            //create customer using bearer token
+
+            var customerInfo = new StringContent(
+                JsonConvert.SerializeObject(
+                    new { Id = 3, BorrowerFirstName = "J", BorrowerLastName = "sd" }),
+                Encoding.UTF8, "application/json");
+
+            var createCustomerResponse = await client.PostAsync("http://localhost:60565/api/Borrowers",
+                customerInfo);
+
+            if(!createCustomerResponse.IsSuccessStatusCode)
+            {
+                Console.WriteLine(createCustomerResponse.StatusCode);
+            }
+
+            //get a list of all of the customers
+            var getCustomerResponse = await client.GetAsync("http://localhost:60565/api/Borrowers");
+            if(!getCustomerResponse.IsSuccessStatusCode)
+            {
+                Console.WriteLine(getCustomerResponse.StatusCode);
+            }
+            else
+            {
+                var content = await getCustomerResponse.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+            Console.Read();
+
+
+
         }
     }
 }
