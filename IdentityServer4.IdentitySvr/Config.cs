07535c6fa2d1d2032ed83d6b463d0379785d3d54
Matt Nygren <MattNygren_7@hotmail.com>
2018-12-21 20:33:44 -0800

    Update Config.cs

--- IdentityServer4.IdentitySvr/Config.cs
+++ IdentityServer4.IdentitySvr/Config.cs
@@ -21,7 +21,25 @@ namespace IdentityServer4.IdentitySvr
         {
             return new List<Client>
             {
-                new Client
+                //You can created multiple different grant types. As of now, we have just a basic client credential grant type A grant type is the way a client can
+                //communicate with the resource (Api etc) and also the way our client can talk to the identity server
+                //Here is a list of possible grant types
+                //Implicit 
+                //ImplicitAndClientCredentials 
+                //Code 
+                //CodeAndClientCredentials  get; 
+                //Hybrid 
+                //HybridAndClientCredentials 
+                //ClientCredentials --most often used in server to server or machine to machine, intranet internal. No user resource involved.
+                //the client sends the secret. so cant use in web based apps. dont use in web based like spa
+                //ResourceOwnerPassword - the resource owner is the user. a user makes use of the client. like mobile device, web site.
+                //in this scenario, not only does the client have to send a client id to identity server, but also sends the user's user name and password, along with 
+                //the client id to the identity server. Identity svr 4 will maintain a list of users. So in the config file, we need to add saving for resource owner.
+                //as of now, just using clientcredentials, we just have the client credentials. we will use asp.net core identity to store the user resource credentials
+                //ResourceOwnerPasswordAndClientCredentials 
+                //DeviceFlow 
+
+        new Client
                 {
                     ClientId="client",
                     AllowedGrantTypes=GrantTypes.ClientCredentials,
